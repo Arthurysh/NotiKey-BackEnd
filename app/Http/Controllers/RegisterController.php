0a9c9ee3db81e2d6149a9d19b4a8bb1890ed8d09
Arthurysh <artur2002236@gmail.com>
2021-12-15 19:56:08 +0200

    Added password generation and sending it to the user by email after being added by the manager

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeMail;
 
 class RegisterController extends Controller
 {
@@ -31,22 +33,43 @@ class RegisterController extends Controller
     }
     public function addUser(Request $request)
     {
+
+        
+            $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            $count = mb_strlen($chars);
+        
+            for ($i = 0, $paslw = ''; $i < 12; $i++) {
+                $index = rand(0, $count - 1);
+                $paslw .= mb_substr($chars, $index, 1);
+            }
+        
+            
+        
+
         $request->validate([
             'name' => ['required'],
             'email' => ['required', 'email', 'unique:users'],
             'surname' => ['required'],
             'phone' => ['required','min:12', 'numeric'],
-            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
-        User::create([
-            'name' => $request->name,
+        
+
+        $data = ([
             'email' => $request->email,
-            'surname' => $request->surname,
-            'birthday' => $request->birthday,
-            'phone' => $request->phone,
-            'password' => Hash::make($request->password),
-            'user_role' => $request->user_role,
+            'password' => ($paslw),
         ]);
+ 
+       Mail::to($request->email)->send(new WelcomeMail($data));
+       User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'surname' => $request->surname,
+        'birthday' => $request->birthday,
+        'phone' => $request->phone,
+        'password' => Hash::make($paslw),
+        'user_role' => $request->user_role,
+    ]); 
+       
     }
 }
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,7 +4,7 @@
  <meta charset=”utf-8">
  </head>
  <body>
- <h2>Test Email</h2>
+ <h2>Добро пожаловать! Ваши данные для входа ниже.</h2>
 <p>{{$data['email']}}</p>
 <p>{{$data['password']}}</p>
 </body>
